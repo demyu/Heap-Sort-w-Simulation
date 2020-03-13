a208943e63399c0296cf9872f7bec0b9e5afca52
demyu <demyujanus@yahoo.com>
2020-03-13 12:58:06 +0800

    First Commit

---
+++ app/Runner.java
@@ -0,0 +1,11 @@
+package app;
+import java.awt.EventQueue;
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+		tempController j = new tempController();
+	}
+}
